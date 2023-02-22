1c04bc00c94dca361a86ec11a8b256e9bbfb9153
Rob <rob@auderenow.org>
2023-02-22 09:40:05 -0800

    Initial commit

---
+++ app/src/main/java/org/auderenow/sdktest/MainActivity.kt
@@ -0,0 +1,27 @@
+package org.auderenow.sdktest
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.auderenow.sdktest.databinding.ActivityMainBinding
+import org.auderenow.camerax.library.CaptureActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        viewBinding.launchCameraButton.setOnClickListener {
+            val myIntent = Intent(this, CaptureActivity::class.java)
+            startActivity(myIntent)
+        }
+
+        viewBinding.showFragmentButton.setOnClickListener {
+            val myIntent = Intent(this, FragmentActivity::class.java)
+            startActivity(myIntent)
+        }
+    }
+}
